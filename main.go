7480dd183c2df089706a1da95523bfcef0df12fa
Alex SHD <alex@shdlabs.com>
2024-05-02 21:43:23 +0300

    clean

--- main.go
+++ main.go
@@ -11,19 +11,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-// func main() {
-// 	timeStringS := "2024-03-15"
-// 	timeStringF := "2024-02-15"
-// 	theTime1, err := time.Parse("2006-01-02", timeStringS)
-// 	if err != nil {
-// 		fmt.Println("Could not parse time:", err)
-// 	}
-// 	theTime2, err := time.Parse("2006-01-02", timeStringF)
-// 	if err != nil {
-// 		fmt.Println("Could not parse time:", err)
-// 	}
-// 	fmt.Println("The time is", theTime1.Before(theTime2))
-//
-// 	fmt.Println(theTime1.Format(time.RFC3339))
-// }
