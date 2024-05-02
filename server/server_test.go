fc6ab83f42647c592d3660fd22f21477135444a8
Alex SHD <alex@shdlabs.com>
2024-05-03 01:01:00 +0300

    more tests

--- server/server_test.go
+++ server/server_test.go
@@ -6,21 +6,13 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 )
-
-// firt endpoint -
-//
-//	JSON payload with 4 fields
-//	- startDate (YYYY-MM-DD)
-//	- endDate
-//	- minCount
-//	- maxCount
 
 func TestCount(t *testing.T) {
 	t.Run("", func(t *testing.T) {
 		a := assert.New(t)
 
+		// testing most used methods
 		for _, method := range []string{http.MethodGet, http.MethodPut, http.MethodDelete} {
 			t.Run(method, func(t *testing.T) {
 				a.HTTPRedirect(countHandler, method, "/count", nil)
@@ -31,36 +23,34 @@ func TestCount(t *testing.T) {
 	t.Run("json play", func(t *testing.T) {
 		a := assert.New(t)
 
-		payload := []byte(`{"startDate": "2024-01-01", "endDate": "2024-02-01", "maxCount": 200, "minCount": 50}`)
-		var dat map[string]any
-		a.NoError(json.Unmarshal(payload, &dat))
-		a.Equal("2024-01-01", dat["startDate"])
+		var (
+			payload = []byte(`
+                  {
+                        "startDate": "2024-01-01",
+                        "endDate": "2024-02-01",
+                        "maxCount": 200,
+                        "minCount": 50
+                  }
+                  `)
+			data map[string]any
+		)
+
+		a.NoError(json.Unmarshal(payload, &data))
+		a.Equal("2024-01-01", data["startDate"])
 		a.HTTPStatusCode(countHandler, "POST", "/count", nil, http.StatusAccepted)
+	})
+
+	t.Run("count docs", func(t *testing.T) {
+		a := assert.New(t)
+
+		a.HTTPSuccess(countDocHandler, http.MethodGet, "/", nil)
 	})
 }
 
-type ServerSuite struct {
-	suite.Suite
-	str string
-}
+func TestMainHandler(t *testing.T) {
+	t.Run("main", func(t *testing.T) {
+		a := assert.New(t)
 
-func (s *ServerSuite) SetupTest() {
-	s.str = "string"
+		a.HTTPSuccess(mainHandler, http.MethodGet, "/", nil)
+	})
 }
-
-func (s *ServerSuite) TestServerSuite() {
-	s.Equal(s.str, "string", "come and fail")
-}
-
-func TestServerSuite(t *testing.T) {
-	suite.Run(t, new(ServerSuite))
-}
-
-// func string2Time(timeStr string) time.Time {
-// 	theTime, err := time.Parse("2006-01-02", timeStr)
-// 	if err != nil {
-// 		slog.Error("time parse failed", err)
-// 	}
-//
-// 	return theTime
-// }
